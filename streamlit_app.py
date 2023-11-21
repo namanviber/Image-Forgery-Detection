fd533189aa8ae758eb3de9e7cec3f94d4b27b87a
Naman Jain <namanviber@gmail.com>
2023-11-21 12:29:13 +0530

    final changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def preprocess_image(img):
     img_gray = img.convert('L')
     img_eq = Image.fromarray(np.array(img_gray), 'L')  # Equalize histogram for grayscale
 
-    img_denoised = img_eq.filter(ImageFilter.DENOISE)
+    img_denoised = img_gray.filter(ImageFilter.GaussianBlur(radius=1))
     img_color_corrected = img_denoised.convert('HSV')
     
     img_array = np.array(img_color_corrected)
@@ -36,12 +36,12 @@ def predict(img):
     pred = pred[0]
     if pred == 0:
         st.image(img_array, caption="Uploaded Image", use_column_width=True)
-        st.success("Forged Image")
-        st.success(f'{prediction[0][0]*100:.2f} % :thumbsup:')
+        st.error("This Image is potentially forged")
+        st.error(f'There is {prediction[0][0]*100:.2f} % chance of image being manupulated')
     else:
         st.image(img_array, caption="Uploaded Image", use_column_width=True)
-        st.info("Image is not Forged")
-        st.info(f'{prediction[0][1]*100:.2f} % :thumbsup:')
+        st.success("This Image is Authentic")
+        st.success(f'There is {prediction[0][1]*100:.2f} % chance that the image is authentic one')
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
