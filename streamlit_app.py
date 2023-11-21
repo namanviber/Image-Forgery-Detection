5d24e49e0b31b729e3f991165e928a8e5bd1d8b9
Naman Jain <90565299+namanviber@users.noreply.github.com>
2023-11-21 06:37:34 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
-import cv2
 from keras.models import load_model
+from PIL import Image, ImageFilter
 
 model = load_model("newMobileNet.h5")
 
@@ -11,39 +11,39 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 # Preprocess Image
 def preprocess_image(img):
-    img = cv2.resize(img, (256, 256))
+    img = img.resize((256, 256))
 
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_eq = cv2.equalizeHist(img_gray)
+    img_gray = img.convert('L')
+    img_eq = Image.fromarray(np.array(img_gray), 'L')  # Equalize histogram for grayscale
 
-    img_denoised = cv2.fastNlMeansDenoising(img_eq, None, h=10, templateWindowSize=7, searchWindowSize=21)
-    img_color_corrected = cv2.cvtColor(img_denoised, cv2.COLOR_GRAY2BGR)
-    img_color_corrected = cv2.cvtColor(img_color_corrected, cv2.COLOR_BGR2HSV)
-    img_color_corrected[:, :, 1] = img_color_corrected[:, :, 1] * 1.2
-    img_color_corrected[:, :, 2] = img_color_corrected[:, :, 2] * 0.8
-    img_color_corrected = cv2.cvtColor(img_color_corrected, cv2.COLOR_HSV2BGR)
+    img_denoised = img_eq.filter(ImageFilter.DENOISE)
+    img_color_corrected = img_denoised.convert('HSV')
+    
+    img_array = np.array(img_color_corrected)
+    img_array[:, :, 1] = img_array[:, :, 1] * 1.2
+    img_array[:, :, 2] = img_array[:, :, 2] * 0.8
+    img_color_corrected = Image.fromarray(img_array, 'HSV').convert('RGB')
 
     return img_color_corrected
 
 # Predict
 def predict(img):
     img = preprocess_image(img)
-    img = img.reshape((1, 256, 256, 3))
+    img_array = np.array(img)
+    img = img_array.reshape((1, 256, 256, 3))
     prediction = model.predict(img)
     pred = np.argmax(prediction, axis=1)
     pred = pred[0]
     if pred == 0:
-        st.image(img, caption="Uploaded Image", use_column_width=True)
+        st.image(img_array, caption="Uploaded Image", use_column_width=True)
         st.success("Forged Image")
         st.success(f'{prediction[0][0]*100:.2f} % :thumbsup:')
     else:
-        st.image(img, caption="Uploaded Image", use_column_width=True)
+        st.image(img_array, caption="Uploaded Image", use_column_width=True)
         st.info("Image is not Forged")
         st.info(f'{prediction[0][1]*100:.2f} % :thumbsup:')
 
 if uploaded_file is not None:
-    image_bytes = uploaded_file.read()
-    image_array = np.asarray(bytearray(image_bytes), dtype=np.uint8)
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
     trigger = st.button('Predict', on_click=lambda: predict(image))
